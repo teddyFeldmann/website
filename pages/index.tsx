98b5012eccd90a169260db127317d31a8b84362a
Teddy Feldmann <teddy.m.feldmann@gmail.com>
2022-12-05 14:12:53 -0500

    adding initial files

---
+++ pages/index.tsx
@@ -0,0 +1,62 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="stylesheet" href="index.css"></link>
+        <title>Teddy Feldmann</title>
+      </Head>
+      <main>
+        <div>
+          <h1>Teddy Feldmann</h1>
+          <div>
+            <p>
+              Student at Georgia Tech studying Computer Science and Materials
+              Science.
+            </p>
+            <p>
+              Researching the mining and resource processing technologies
+              underpinning our transition to a net zero-carbon economy.
+            </p>
+            <p>
+              Tweeting about mining, America, and my views on the world{" "}
+              <a href="https://twitter.com/teddyfeld" target="_blank">
+                here
+              </a>
+              , and writing about them soon at{" "}
+              <a href="https://www.americanaegis.us" target="_blank">
+                American Aegis
+              </a>
+              .
+            </p>
+            <p>
+              Reach out to me on{" "}
+              <a href="https://twitter.com/teddyfeld" target="_blank">
+                Twitter
+              </a>
+              ,{" "}
+              <a
+                href="https://www.linkedin.com/in/teddyfeldmann/"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+              , or{" "}
+              <a href="mailto:teddymfeldmann@gmail.com" target="_blank">
+                email
+              </a>
+              .
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Home
